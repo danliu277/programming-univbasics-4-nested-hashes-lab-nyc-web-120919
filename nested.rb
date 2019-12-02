fbde939c44c8c32a8cfea222915f2c09e7f7c372
Daniel Liu <danliu277@gmail.com>
2019-12-02 20:55:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,7 +112,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     puts(programmer_hash[:alan_kay][:known_for])
+     puts(programmer_hash[:alan_kay])
 end
 
 def adding_to_dennis
